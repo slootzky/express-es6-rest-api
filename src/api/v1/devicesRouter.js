ffce5490b0b8f3897c15ff5d947ddf6931129b63
asafkotzer <asaf@soluto.com>
2018-12-11 11:07:02 +0200

    new sgate object

--- src/api/v1/devicesRouter.js
+++ src/api/v1/devicesRouter.js
@@ -15,12 +15,13 @@ devicesStateRoute.get('/:deviceId', async ({ params: { deviceId } }, res) => {
   err ? res.status(404).send(err) : res.json(deviceState);
 });
 
-devicesStateRoute.patch('/:deviceId/:attribute', async ({ body, params: { deviceId, attribute } }, res) => {
+devicesStateRoute.patch('/:deviceId/:state', async ({ body, params: { deviceId, state } }, res) => {
   const deviceIndex = (await getDevicesState()).findIndex(deviceState => deviceState.deviceId == deviceId);
   if (deviceIndex > -1) {
     const oldState = (await getDevicesState())[deviceIndex];
-    const stateChange = { attributes: { [state]: { value: body } } };
-    const newState = { ...oldState, ...stateChange };
+    const stateChange = { [state]: { value: body} };
+    const newStateObject = { ...oldState[state], ...stateChange[state] }
+    const newState = { ...oldState, ...newStateObject };
     await switchDeviceState(deviceIndex, newState);
     res.sendStatus(200);
   } else {
