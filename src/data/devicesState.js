d99a3997b51b9820390845479b50c25ea9132109
Rotem Slootzky <rot253@gmail.com>
2018-12-11 12:32:56 +0200

    added range possible values

--- src/data/devicesState.js
+++ src/data/devicesState.js
@@ -2,14 +2,14 @@ let devicesState = [
   {
     deviceId: '1',
     state: { value: 'cool', type: 'picker', values: ['cool', 'warm', 'fan', 'off'] },
-    temperature: { value: '19', type: 'range' },
+    temperature: { value: '19', type: 'range', values: { min: 0, max: 100 } },
   },
   { deviceId: '2', switch: { value: 'on', type: 'switch' } },
   { deviceId: '3', switch: { value: 'off', type: 'switch' } },
   {
     deviceId: '4',
     state: { value: 'warm', type: 'picker', values: ['cool', 'warm', 'fan', 'off'] },
-    temperature: { value: '26', type: 'range' },
+    temperature: { value: '26', type: 'range', values: { min: 0, max: 100 } },
   },
 ];
 
